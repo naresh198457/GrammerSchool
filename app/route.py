ceaa72e5724bde4ac860752ef3cccac932f3e4eb
naresh198457 <naresh.sampara@gmail.com>
2025-01-02 15:19:10 +0000

    removed login and regis

--- app/route.py
+++ app/route.py
@@ -2,7 +2,6 @@ from app import app
 from flask import render_template, request, url_for, flash, redirect, session
 import os
 from flask_mail import Mail, Message
-from itsdangerous import URLSafeTimedSerializer
 from app.models import db
 from app.models import User, dailyActivity
 from app.forms import RegistrationForm, LoginForm, contactForm, practiceform, commonletterquestionform, findnextletterpairform
@@ -12,137 +11,22 @@ from flask_login import login_user, current_user, logout_user, login_required
 import pandas as pd
 from datetime import datetime, timedelta
 
-mail=Mail(app)
-s=URLSafeTimedSerializer('Thisissecret!')
 
 # login page
 @app.route('/')
-@app.route('/login',methods=['POST','GET'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form=LoginForm()
-    if form.validate_on_submit():
-        user=User.query.filter_by(username=form.username.data).first()
-        if user and check_password_hash(user.password,form.password.data):
-            login_user(user,remember=form.remember.data)
-            next_page=request.args.get('next')
-            #flash(f'{user.firstname} have been logged in!','success')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
-        else:
-            flash('Login Unsuccessful. please check the username and password','danger')
-            print('Login unsucessful')
-    return render_template('login.html', title='Login',form=form)
-
-# register page
-@app.route('/register',methods=['GET','POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        # adding the new data to the database
-        hash_password=generate_password_hash(password=form.password.data).decode('utf-8')
-        user=User(username=form.username.data,
-                  email=form.email.data,
-                  firstname=form.firstname.data,
-                  lastname=form.lastname.data,
-                  password=hash_password)
-        db.session.add(user)
-        db.session.commit()
-
-        # send the conformation email to the user
-        email=form.email.data
-
-        msg=Message('Home Work -- registeration email',
-                    recipients=[email],
-                    sender='naresh.sampara@gmail.com',
-                    html=render_template('Email_coformation.html',
-                                         UserLastName=form.lastname.data.capitalize(),
-                                         UserFirstName=form.firstname.data.capitalize(),
-                                         Username=form.username.data,
-                                         UserEmail=form.email.data))
-        #mail.send(msg)
-        
-        flash(f'Account created for {form.username.data}!','success')
-        return redirect(url_for('login'))
-    return render_template('register.html', title='register',form=form,)
-
-# home page
 @app.route('/home',methods=['POST','GET']) # decorators: '/' route page of our website
-@login_required
 def home():
     image_file=url_for('static',filename='profile_pics/'+current_user.image_file)
     return render_template('home.html',image_file=image_file)
 
-# account page 
-@app.route('/account')
-@login_required
-def account():
-    image_file=url_for('static',filename='profile_pics/'+current_user.image_file)
-
-    user_activities = dailyActivity.query.filter_by(username=current_user.username).all()
-    data = [{
-        "id": activity.id,
-        "username": activity.username,
-        "date_test":activity.date_test,
-        "testType": activity.testType,
-        "noQuestion": activity.noQuestion,
-        "correctAns": activity.correctAns
-    } for activity in user_activities]
-
-    df = pd.DataFrame(data)
-
-    if df.empty:
-        return render_template('home.html')
-    else: 
-        test_types = df["testType"].unique()
-        selected_test_type = request.form.get("testType", 'All')
-
-        if selected_test_type == 'All':
-            filtered_df = df
-        else:
-            filtered_df = df[df['testType']==selected_test_type]
-
-        bar_chart = create_bar_chart(filtered_df)
-        pie_chart = create_pie_chart(filtered_df)
-        
-        return render_template('account.html',
-                            image_file=image_file,
-                            test_types = test_types,
-                            bar_chart = bar_chart,
-                            pie_chart = pie_chart,
-                            selected_test_type = selected_test_type)
-
-# Logout route
-@app.route("/logout")
-def logout():
-    logout_user()
-    return redirect(url_for('login'))
 
 # about route
 @app.route("/about",methods=['GET','POST'])
 def about():
 
-    form=contactForm()
     Designer_im_file=url_for('static',filename='Desiner_Pics/Naresh_BW.jpg')
-    if request.method == 'POST':
-        if form.validate() == False:
-            flash('All fields are required.')
-            return render_template('about.html', form=form)
-        else:
-            msg=Message(form.Subject.data,
-                        sender='naresh.sampara@gmail.com',
-                        recipients=['dr.sampara@gmail.com'],
-                        html=render_template('Contact_Enquire.html',
-                                         UserName=form.Name.data.capitalize(),
-                                         UserEmail=form.Email.data,
-                                         UserMessage=form.Message.data))
-            mail.send(msg)
-            return render_template('about.html',success=True)
-    elif request.method == 'GET':
-        
-        return render_template('about.html',Designer_im_file=Designer_im_file,form=form)
+    
+    return render_template('about.html',Designer_im_file=Designer_im_file)
     
 
 # Practice route
@@ -198,13 +82,7 @@ def commonletter():
     global CL_questions_1, CL_questions_2, CL_Answers, CL_urAnswers  
 
     if cl_q_no >= len(df_cl):
-        Activity=dailyActivity(username=current_user.username,
-                               testType='CommonLetter',
-                               noQuestion=cl_q_no,
-                               correctAns=cl_a_no)
-                
-        db.session.add(Activity)
-        db.session.commit()
+
         return redirect(url_for('commonletterdashboard'))
 
     question_data = df_cl.iloc[cl_q_no]
@@ -260,13 +138,7 @@ def nextletterpair():
     global FNPL_letters_1, FNPL_letters_2, FNPL_letters_3, FNPL_letters_4, FNPL_Answers, FNPL_urAnswers
 
     if fnpl_q_no >= len(df_fnpl):
-        Activity=dailyActivity(username=current_user.username,
-                               testType='findnextletterpair',
-                               noQuestion=fnpl_q_no,
-                               correctAns=fnpl_a_no)
-                
-        db.session.add(Activity)
-        db.session.commit()
+
         return redirect(url_for('nextletterpairdashboard'))
 
     question_data = df_fnpl.iloc[fnpl_q_no]
